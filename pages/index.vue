18205e06f93bc7f1a41fb078fee3239566db638f
NamSnow <phamhoainam08102004@gmail.com>
2025-06-11 16:50:23 +0700

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,61 @@
 <template>
-  <div class="container">
-    <div class="add-task-section">
-      <input
-        v-model="newTask"
-        type="text"
-        placeholder="Thêm công việc mới..."
-        @keyup.enter="addTask()"
-      />
-      <button @click="addTask()">Add</button>
+  <div class="add-task-section">
+    <input
+      v-model="newTask"
+      type="text"
+      placeholder="Thêm công việc mới..."
+      @keyup.enter="addTask()"
+    />
+    <button @click="addTask()">Add</button>
+  </div>
+
+  <div class="task-management-section">
+    <div class="search-filter-row">
+      <div>
+        <input
+          type="search"
+          placeholder="Tìm kiếm công việc..."
+          v-model="searchTerm"
+        />
+        <button>Search</button>
+      </div>
+
+      <div>Kết quả tìm kiếm: {{ filteredAndSearchedTasks.length }}</div>
     </div>
 
-    <div class="task-management-section">
-      <div class="search-filter-row">
-        <div>
-          <input
-            type="search"
-            placeholder="Tìm kiếm công việc..."
-            v-model="searchTerm"
-          />
-          <button>Search</button>
-        </div>
-
-        <div>Kết quả tìm kiếm: {{ filteredAndSearchedTasks.length }}</div>
+    <div class="tasks-overview">
+      <h2>Tasks</h2>
+      <div class="task-count">
+        <img src="~/public/tasks-app.svg" alt="tasks icon" />
+        <div>{{ completedTask }} / {{ tasks.length }}</div>
       </div>
+      <select v-model="selectedFilter">
+        <option value="">--Chọn--</option>
+        <option value="incomplete">Chưa hoàn thành</option>
+        <option value="completed">Đã hoàn thành</option>
+      </select>
+    </div>
 
-      <div class="tasks-overview">
-        <h2>Tasks</h2>
-        <div class="task-count">
-          <img src="~/public/tasks-app.svg" alt="tasks icon" />
-          <div>{{ completedTask }} / {{ tasks.length }}</div>
+    <div
+      class="task-list"
+      v-for="(task, index) in filteredAndSearchedTasks"
+      :key="index"
+    >
+      <div class="task-item">
+        <div
+          v-if="!task.isEditing"
+          :class="task.complTask ? 'line-through text-gray-700' : ''"
+        >
+          {{ task.text }}
         </div>
-        <select v-model="selectedFilter">
-          <option value="">--Chọn--</option>
-          <option value="incomplete">Chưa hoàn thành</option>
-          <option value="completed">Đã hoàn thành</option>
-        </select>
-      </div>
-
-      <div
-        class="task-list"
-        v-for="(task, index) in filteredAndSearchedTasks"
-        :key="index"
-      >
-        <div class="task-item">
-          <div
-            v-if="!task.isEditing"
-            :class="task.complTask ? 'line-through text-gray-700' : ''"
-          >
-            {{ task.text }}
-          </div>
-          <input v-else type="text" v-model="task.text" />
-          <button @click="buttonComplTask(task)">
-            {{ task.complTask ? "Undo" : "Compl" }}
-          </button>
-          <button @click="task.isEditing ? saveTask(task) : editTask(task)">
-            {{ task.isEditing ? "Save" : "Edit" }}
-          </button>
-          <button @click="deleteTask(index)">Delete</button>
-        </div>
+        <input v-else type="text" v-model="task.text" />
+        <button @click="buttonComplTask(task)">
+          {{ task.complTask ? "Undo" : "Compl" }}
+        </button>
+        <button @click="task.isEditing ? saveTask(task) : editTask(task)">
+          {{ task.isEditing ? "Save" : "Edit" }}
+        </button>
+        <button @click="deleteTask(index)">Delete</button>
       </div>
     </div>
   </div>
